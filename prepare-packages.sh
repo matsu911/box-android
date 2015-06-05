782dc272321b77f958dd746f76d8368073a5efca
Shigeaki Matsumura <matsu911@gmail.com>
2015-06-05 22:19:13 +0900

    Update prepare-packages.sh

--- prepare-packages.sh
+++ prepare-packages.sh
@@ -3,4 +3,5 @@
 sudo apt-get update -qq
 sudo apt-get install -qq --force-yes expect
 sudo apt-get install -qq --force-yes libgd2-xpm ia32-libs ia32-libs-multiarch
+sudo apt-get install -qq --force-yes nodejs npm
 
